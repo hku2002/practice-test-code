9be8ab4d3b65986ab4a6e7bf4c1c9208e239ee37
Hwang <hku2002@hanmail.net>
2023-09-22 20:29:13 +0900

    refactor: displayName 구체화

--- cafekiosk/src/test/java/com/sample/cafekiosk/unit/beverage/AmericanoTest.java
+++ cafekiosk/src/test/java/com/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -8,14 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AmericanoTest {
 
     @Test
-    @DisplayName("이름이 아메리카노인지 확인")
+    @DisplayName("Americano 의 이름을 조회하면 '아메리카노' 가 나와야 한다.")
     void getNameTest() {
         Americano americano = new Americano();
         assertThat(americano.getName()).isEqualTo("아메리카노");
     }
 
     @Test
-    @DisplayName("가격이 올바른지 확인")
+    @DisplayName("Americano 의 가격은 4,000원 이어야 한다.")
     void getPriceTest() {
         Americano americano = new Americano();
         assertThat(americano.getPrice()).isEqualTo(4_000);
