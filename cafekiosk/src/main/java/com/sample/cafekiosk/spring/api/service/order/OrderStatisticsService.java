a2feab19977393632956a370cf642593d5ccbb09
Hwang <hku2002@hanmail.net>
2023-09-24 23:29:12 +0900

    refactor: 기능별 분리

--- cafekiosk/src/main/java/com/sample/cafekiosk/spring/api/service/order/OrderStatisticsService.java
+++ cafekiosk/src/main/java/com/sample/cafekiosk/spring/api/service/order/OrderStatisticsService.java
@@ -19,27 +19,30 @@ public class OrderStatisticsService {
     private final MailService mailService;
 
     public boolean sendOrderStatisticsMail(LocalDate orderDate, String email) {
-        // 해당 일자에 결제완료된 주문들을 가져온다.
-        List<Order> orders = orderRepository.findOrdersBy(
-                orderDate.atStartOfDay(),
-                orderDate.plusDays(1).atStartOfDay(),
-                PAYMENT_COMPLETED
-        );
+        List<Order> orders = findPaymentCompletedOrders(orderDate);
 
-        // 총 매출 합계를 계산한다.
-        int totalAmount = orders.stream()
-                .mapToInt(Order::getTotalPrice)
-                .sum();
-
-        // 메일을 전송한다.
         boolean sendResult = mailService.sendMail(
                 "no-reply@cafe-kiosk.com"
                 , email
                 , String.format("[매출통계] %s", orderDate)
-                , String.format("총 매출 합계 : %s 원", totalAmount));
+                , String.format("총 매출 합계 : %s 원", calculateTotalAmount(orders)));
 
         if (!sendResult) throw new IllegalStateException("매출 통계 메일 전송에 실패하였습니다.");
 
         return true;
+    }
+
+    private List<Order> findPaymentCompletedOrders(LocalDate orderDate) {
+        return orderRepository.findOrdersBy(
+                orderDate.atStartOfDay(),
+                orderDate.plusDays(1).atStartOfDay(),
+                PAYMENT_COMPLETED
+        );
+    }
+
+    private int calculateTotalAmount(List<Order> orders) {
+        return orders.stream()
+                .mapToInt(Order::getTotalPrice)
+                .sum();
     }
 }
