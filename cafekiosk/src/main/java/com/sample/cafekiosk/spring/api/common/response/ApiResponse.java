fb9abd51447bc44a2c260d155874e9225d0fb1c8
Hwang <hku2002@hanmail.net>
2023-09-24 12:39:22 +0900

    feat: ok, created 메소드 추가

--- cafekiosk/src/main/java/com/sample/cafekiosk/spring/api/common/response/ApiResponse.java
+++ cafekiosk/src/main/java/com/sample/cafekiosk/spring/api/common/response/ApiResponse.java
@@ -8,4 +8,23 @@ public class ApiResponse<T> {
     private HttpStatus status;
     private String message;
     private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return new ApiResponse<>(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(HttpStatus.OK, HttpStatus.OK.name(), data);
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(HttpStatus.CREATED, HttpStatus.CREATED.name(), data);
+    }
 }
