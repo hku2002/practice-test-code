f2144feb099c2b939d09e5310fe25ddb3b01949f
Hwang <hku2002@hanmail.net>
2023-09-28 01:13:09 +0900

    test: User 테스트코드 최초 추가

---
+++ secondhand-commerce/src/test/java/com/secondhand/api/user/domain/UserTest.java
@@ -0,0 +1,29 @@
+package com.secondhand.api.user.domain;
+
+import com.secondhand.api.common.domain.Address;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.secondhand.api.user.domain.enumtype.UserStatus.ACTIVE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @Test
+    @DisplayName("유저 생성 시 status 값은 ACTIVE 이다.")
+    void createStatusActiveTest() {
+        // given
+        Address address = Address.builder()
+                .address("서울시 강남구 강남로 100")
+                .addressDetail("테스트 빌딩 2005호")
+                .build();
+
+        // when
+        User user = User.create("홍길동", "01012341234", address);
+
+        // then
+        assertThat(user.getStatus()).isEqualTo(ACTIVE);
+
+    }
+
+}
