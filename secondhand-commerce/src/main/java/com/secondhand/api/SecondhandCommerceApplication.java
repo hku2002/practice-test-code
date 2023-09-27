cec3643578b3f29105b04146d2379f5081097177
Hwang <hku2002@hanmail.net>
2023-09-28 00:34:30 +0900

    feat: 중고거래 커머스 최초 생성

---
+++ secondhand-commerce/src/main/java/com/secondhand/api/SecondhandCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.secondhand.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandCommerceApplication.class, args);
+    }
+
+}
---
+++ secondhand-commerce/src/test/java/com/secondhand/api/SecondhandCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondhand.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
