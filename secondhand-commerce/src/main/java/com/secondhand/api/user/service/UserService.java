1f5dee883cc582c458b215e13aec97645475089e
Hwang <hku2002@hanmail.net>
2023-09-28 02:08:52 +0900

    feat: UserService 최초 생성

---
+++ secondhand-commerce/src/main/java/com/secondhand/api/user/service/UserService.java
@@ -0,0 +1,31 @@
+package com.secondhand.api.user.service;
+
+import com.secondhand.api.common.domain.Address;
+import com.secondhand.api.user.domain.User;
+import com.secondhand.api.user.domain.UserRepository;
+import com.secondhand.api.user.dto.UserCreateRequest;
+import com.secondhand.api.user.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserResponse createUser(UserCreateRequest request) {
+        Address address = request.toAddress(
+                request.getAddress(),
+                request.getAddressDetail(),
+                request.getZipCode()
+        );
+
+        User user = User.create(request.getName(), request.getPhoneNumber(), address);
+        return UserResponse.of(user);
+    }
+
+}
