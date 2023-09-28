da22970abafeafe95ec813418f74702983761c89
Hwang <hku2002@hanmail.net>
2023-09-28 19:23:33 +0900

    test: 로그인 기능 테스트 최초 추가

--- secondhand-commerce/src/test/java/com/secondhand/api/user/service/UserServiceTest.java
+++ secondhand-commerce/src/test/java/com/secondhand/api/user/service/UserServiceTest.java
@@ -1,6 +1,10 @@
 package com.secondhand.api.user.service;
 
+import com.secondhand.api.common.domain.Address;
+import com.secondhand.api.user.domain.User;
 import com.secondhand.api.user.domain.UserRepository;
+import com.secondhand.api.user.dto.LoginRequest;
+import com.secondhand.api.user.dto.TokenResponse;
 import com.secondhand.api.user.dto.UserCreateRequest;
 import com.secondhand.api.user.dto.UserResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -27,6 +31,8 @@ class UserServiceTest {
     void createUserCorrectReturn() {
         // given
         UserCreateRequest request = UserCreateRequest.builder()
+                .email("hong@sample.com")
+                .password("test-pass-1234$")
                 .name("홍길동")
                 .phoneNumber("01012345678")
                 .address("서울특별시 강남구 강남로 100")
@@ -42,6 +48,29 @@ class UserServiceTest {
                 () -> assertThat(userResponse.getPhoneNumber()).isEqualTo("01012345678"),
                 () -> assertThat(userResponse.getAddress()).isEqualTo("서울특별시 강남구 강남로 100")
         );
+    }
+
+    @Test
+    @DisplayName("로그인이 올바르게 동작한다.")
+    void loginTest() {
+        // given
+        LoginRequest request = LoginRequest.builder()
+                .email("hong@sample.com")
+                .password("test-password-1234!")
+                .build();
+        Address address = Address.builder()
+                .address("서울특별시 강남구 강남로 100")
+                .addressDetail("좋은 빌딩 1001호")
+                .zipCode("01000")
+                .build();
+        User user = User.create("hong@sample.com", "test-pass-1234!", "홍길동", "01012345678", address);
+        userRepository.save(user);
+
+        // when
+        TokenResponse response = userService.login(request);
+
+        // then
+        assertThat(response.getToken()).isNotEmpty();
     }
 
 }
