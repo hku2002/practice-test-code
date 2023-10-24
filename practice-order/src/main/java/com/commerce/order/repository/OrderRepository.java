fccdccb9759b75cf2d63aab1f40e1f3f6f6c91aa
Hwang <hku2002@hanmail.net>
2023-10-24 22:02:50 +0900

    feat: entity 및 repository 추가

---
+++ practice-order/src/main/java/com/commerce/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.commerce.order.repository;
+
+import com.commerce.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ practice-order/src/main/java/com/commerce/product/entity/Product.java
@@ -0,0 +1,35 @@
+package com.commerce.product.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+
+    private int price;
+
+    private int quantity;
+
+    @Builder
+    public Product(Long id, String productName, int price, int quantity) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
